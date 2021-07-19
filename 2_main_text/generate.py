b2d2392b0b2def6412a015a6dbcd249d543eec23
matanki.saito <matanki.saito@gmail.com>
2021-07-19 23:05:36 +0900

    first commit

---
+++ 2_main_text/generate.py
@@ -0,0 +1,60 @@
+import os
+import subprocess
+
+
+def generate_bm_font(name, ext, add_source_file):
+    # コマンド
+    cmd_list = [
+        '../1_tool/bmfont64.exe',
+        '-c', "./bmfc/" + name + ext,
+        '-o', "./out/" + name + ".fnt"
+    ]
+
+    # 追加ファイルあり
+    if add_source_file:
+        cmd_list.append('-t')
+        cmd_list.append(add_source_file)
+
+    # BFFont実行
+    subprocess.call(" ".join(cmd_list))
+
+
+def read_text_file(name):
+    f = open('./source/' + name + '.txt', "r", encoding='utf_8_sig')
+    text = f.read()
+    f.close()
+    return text
+
+
+def main():
+    source_text = ""
+
+    # sourceフォルダ内を走査
+    files = os.listdir("./source")
+    for file in files:
+        # 拡張子とファイル名を分離
+        file_name, file_ext = os.path.splitext(file)
+
+        # 拡張子がtxt
+        if file_ext == ".txt":
+            source_text = source_text + read_text_file(file_name)
+
+    # source textを作成
+    f = open('source.txt', 'w', encoding='utf_8_sig')
+    f.write(source_text)
+    f.close()
+
+    # bmfcフォルダ内を走査
+    files = os.listdir("./bmfc")
+    for file in files:
+        # 拡張子とファイル名を分離
+        file_name, file_ext = os.path.splitext(file)
+
+        # 拡張子がbmfcの時のみフォントを生成
+        if file_ext == ".bmfc":
+            generate_bm_font(file_name, file_ext, "source.txt")
+
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()
---
+++ README.md
@@ -0,0 +1,31 @@
+# CK2 fontcreate
+
+## Overview
+
+- Steam配信されているWindows版Victoria IIの日本語化パッチの副産物です。パッチはこのプログラムを使って作成されたフォントのみを受け付けます。
+- CP1252の0x80から0x9Fまでに相当する文字（例えば€）はUCS2のコードポイントではなくて、CP1252のコードポイントとして作成されます。
+- 0x100から0x1FFまでの文字は私用領域(0xE000)にシフトされたコードポイントになります。例えばλは3BBではなくて、0xE3BBのコードポイントになります。
+
+## create on local
+
+### Requirement
+
+- Windows 10 or later
+- python 3.5 or later
+- pillow ```pip install pillow```
+
+### Usage
+
+1. 同梱している1_tool/bmfont64.exeを使用して、作成したいフォントのconfigurationファイル（.bmfc）を2_main_text/bmfcフォルダに保存してください。この時、含める文字は空にしてください。
+1. 含めたい文字を2_main_text/sourceフォルダに任意の名前のBOM付きのUTF-8のtxtファイルに入れておきます。こちらで予めCP1252の追加文字と大体のSHIFT_JISの範囲の文字を入れたテキストを用意していますので、これ以外で必要な文字をtxtとして追加してください。
+1. 準備が整ったら、2_main_text/generate.pyを実行します。2_main_text/outフォルダに生成されたフォントが入ります。
+1. 3_map_textと4_capital_textについても同様に実施してください。
+
+## Licence
+
+- generate.py : MIT license
+- bmfont64.exe : zlib license
+- [源ノ明朝](https://github.com/adobe-fonts/source-han-serif) : SIL Open Font License 1.1
+- [源ノ角ゴシック](https://github.com/adobe-fonts/source-han-sans) : SIL Open Font License 1.1
+- nvcompress: MIT license
+- Add-Font.ps1 : free
